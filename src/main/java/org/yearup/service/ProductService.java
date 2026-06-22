e2a4417dcb44c196526da88f6b9e41e501cc0b5f
yuujn <pale.cup5223@fastmail.com>
2026-06-22 15:18:16 -0500

    make product update include stock

--- src/main/java/org/yearup/service/ProductService.java
+++ src/main/java/org/yearup/service/ProductService.java
@@ -56,6 +56,7 @@ public class ProductService
         existing.setSubCategory(product.getSubCategory());
         existing.setFeatured(product.isFeatured());
         existing.setImageUrl(product.getImageUrl());
+        existing.setStock(product.getStock());
         return productRepository.save(existing);
     }
 
