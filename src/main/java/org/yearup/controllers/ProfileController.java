500a3d070d4444e86a7ef3e7acea741afca7da77
yuujn <pale.cup5223@fastmail.com>
2026-06-23 15:06:20 -0500

    add GET profile

---
+++ src/main/java/org/yearup/controllers/ProfileController.java
@@ -0,0 +1,33 @@
+package org.yearup.controllers;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.yearup.exception.NotFoundException;
+import org.yearup.models.Profile;
+import org.yearup.models.User;
+import org.yearup.service.ProfileService;
+import org.yearup.service.UserService;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/profile")
+public class ProfileController {
+    private final ProfileService profileService;
+    private final UserService userService;
+
+    public ProfileController(ProfileService profileService, UserService userService) {
+        this.profileService = profileService;
+        this.userService = userService;
+    }
+
+    @GetMapping
+    @PreAuthorize("hasRole('ROLE_USER')")
+    public Profile getProfile(Principal principal) {
+        User user = userService.getLoggedInUser(principal);
+        return profileService.getProfileByUserId(user.getId())
+                .orElseThrow(NotFoundException::new);
+    }
+}
--- src/main/java/org/yearup/service/ProfileService.java
+++ src/main/java/org/yearup/service/ProfileService.java
@@ -4,18 +4,21 @@ import org.springframework.stereotype.Service;
 import org.yearup.models.Profile;
 import org.yearup.repository.ProfileRepository;
 
+import java.util.Optional;
+
 @Service
-public class ProfileService
-{
+public class ProfileService {
     private final ProfileRepository profileRepository;
 
-    public ProfileService(ProfileRepository profileRepository)
-    {
+    public ProfileService(ProfileRepository profileRepository) {
         this.profileRepository = profileRepository;
     }
 
-    public Profile create(Profile profile)
-    {
+    public Profile create(Profile profile) {
         return profileRepository.save(profile);
+    }
+
+    public Optional<Profile> getProfileByUserId(int userId) {
+        return profileRepository.findById(userId);
     }
 }
